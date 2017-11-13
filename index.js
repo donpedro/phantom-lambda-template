2da7a671c8d6d1c908044185530e6a95b91de470
Peter Lambert <peterl@sbcotton.com>
2017-11-13 17:37:34 -0600

    Switching to binary PDF export, add correct Content-Type header  tweak body input to expect an object (instead of string representation of the object) Add debugging code

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-
 var pdf = require('html-pdf');
 var AWS = require('aws-sdk');
+// var fs = require('fs'); // for debug file writes below
 
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
-var options = { format: 'Letter'/*, phantomPath: './phantomjs_lambda/phantomjs_linux-x86_64' */};
+var options = { format:'Letter',
+    "border": {
+        "top": ".4in",            // default is 0, units: mm, cm, in, px
+        "right": ".25in",
+        "bottom": ".25in",
+        "left": ".25in"
+    },
+    "orientation": "portrait"
+};
 //var S3config = { bucketName: 'your-bucket' }; //Change to your bucket name
 
 exports.handler = function(event, context, callback) {
   //Get the values from the request
   console.log("event: " + JSON.stringify(event));
   
-  const body = JSON.parse(event.body);
+  const body = event.body;
   var htmlString = body.htmlString;
+  Object.assign(options, body.options); // merging an options object that was (possibly) passed in with our default options object
+
+  //console.log("htmlString: " + htmlString);
+  //fs.writeFile('test.htm', htmlString);
   var fileName = event.fileName;
 
   //Create the PDF file from the HTML string
@@ -24,12 +36,20 @@ exports.handler = function(event, context, callback) {
         callback(error);
       }
       else {
+        // console.log(buffer);
+        // fs.writeFile('test.pdf', buffer);
         callback(null, {
             statusCode: 200,
             headers: {
-              'Access-Control-Allow-Origin': '*',
+              // TODO: limit to a whitelist of allowed sites
+              "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+              "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS 
+              "Content-Type" : "application/pdf"
             },
-            body: buffer.toString('base64')
+            //body: buffer.toString('base64') // <-- now returning the buffer as binary instead of converting to text here
+            // For AWS Api Gateway be sure to customize the gateway to allow for binary return values; 
+            // see https://aws.amazon.com/blogs/compute/binary-support-for-api-integrations-with-amazon-api-gateway/
+            body: buffer
           });          
 /*          
         var s3 = new AWS.S3();
